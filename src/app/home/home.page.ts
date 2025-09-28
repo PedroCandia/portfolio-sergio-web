ede1d3638cd7d45bc6ecd64112fc28c982c975e0
Pedro Candia <petercandiavazquez@gmail.com>
2025-09-28 12:31:20 -0600

    Funcionalidad toolbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { FooterComponent } from '../components/footer/footer.component';
 import { HeaderComponent } from '../components/header/header.component';
 
@@ -7,8 +7,17 @@ import { HeaderComponent } from '../components/header/header.component';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FooterComponent, HeaderComponent],
+  imports: [IonContent, FooterComponent, HeaderComponent],
 })
 export class HomePage {
+  isShrunk = false;
+
   constructor() {}
+
+  onScroll(event: any) {
+    this.isShrunk = event.detail.scrollTop > 70; // a los 50px cambia
+
+    console.log(this.isShrunk);
+    
+  }
 }
