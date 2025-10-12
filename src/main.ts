45cf389368db88cff27abdb06ebeba2a89e6fa0f
Pedro Candia <petercandiavazquez@gmail.com>
2025-10-11 20:28:11 -0600

    a lot of configs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    // 🔹 Ionic routing + animation + Angular core modules
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideAnimations(),
+    importProvidersFrom(BrowserModule, IonicModule.forRoot({})),
   ],
-});
+}).catch(err => console.error(err));
